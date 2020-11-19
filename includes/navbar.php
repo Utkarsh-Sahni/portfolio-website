5dec2a65bd1a9a5c22a48b9951deaced52eaba5f
Utkarsh Sahni <utkarshsahni66@gmail.com>
2020-11-19 13:06:46 +0530

    php to html

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,7 +3,7 @@
             <nav style="color: #2d48ec;" class="navbar navbar-expand-md">
               <a class="navbar-brand" href="#"><img src="utkarsh-logo.png" id="logo" class="img-fluid" alt="Logo"></a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fas fa-bars"></span>
+                <span class="navbar-toggler-icon"></span>
               </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav ml-auto">
